ea9bb06487e48f99a6e6c1aa3191936492b6aa5a
sarek1973255-cloud <sarek1973255@gmail.com>
2026-05-13 14:27:51 +0500

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,6 +1,18 @@
-import pandas as pd
+import openpyxl
 
-def save_excel(data, filename="clients.xlsx"):
-    df = pd.DataFrame(data)
-    df.to_excel(filename, index=False)
-    return filename
+def save_excel(data):
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    ws.append(["Company", "Info"])
+
+    for item in data:
+        ws.append([
+            item.get("company", ""),
+            item.get("info", "")
+        ])
+
+    file_name = "results.xlsx"
+    wb.save(file_name)
+
+    return file_name
